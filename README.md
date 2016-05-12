b1c4b1de424eee4ddf3748a4de0a6b5d43261d32
hootsieroll <jayhoots@gmail.com>
2016-05-12 13:28:36 -0400

    update to redux todomvc

--- README.md
+++
@@ -1,7 +0,0 @@
-###Getting Started###
-
-There are two methods for getting started with this repo.
-```
-	> npm install
-	> npm start
-```
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
+var path = require('path')
+var webpack = require('webpack')
+
 module.exports = {
+  devtool: 'cheap-module-eval-source-map',
   entry: [
-    './src/index.js'
+    'webpack-hot-middleware/client',
+    './index'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.css?$/,
+        loaders: [ 'style', 'raw' ],
+        include: __dirname
+      }
+    ]
   }
-};
+}
